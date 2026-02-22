9c7db2a2519445adbffdb7e22e3930a7427ab0a4
ilia <fedorenkoilia@gmail.com>
2026-02-22 14:08:42 +0100

    feat: Add Healthcheck router

--- app/router/router.py
+++ app/router/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.router.auth import router as auth_router
 from app.router.users import router as users_router
+from app.router.shared import router as shared_router 
 
 
 api_router = APIRouter()
+api_router.include_router(shared_router)
 api_router.include_router(auth_router)
 api_router.include_router(users_router)
--- app/router/shared.py
+++ app/router/shared.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 
-from app.routers.api.base import Router
+from app.router import Router
 from app.schemas.shared import GetHealthcheckResponse
 
 
